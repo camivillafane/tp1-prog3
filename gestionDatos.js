db183d5d01b2bd061e30031ecaab65d8f9fbd763
cami <camichwan@gmail.com>
2025-08-14 21:35:15 -0300

    primeros puntos del trabajo practico

---
+++ gestionDatos.js
@@ -0,0 +1,12 @@
+let productos = [
+    {id: 1, nombre: "Hamburguesa", precio: 9000, stock: 45},
+    {id: 2, nombre: "Papas fritas", precio: 3500, stock: 30},
+    {id: 3, nombre: "Pizza", precio: 12000, stock: 35},
+    {id: 4, nombre: "Helado", precio: 4000, stock: 0},
+    {id: 5, nombre: "Flan con DDL", precio: 3000, stock: 55}
+]
+
+console.log("\nOperaciones básicas y Acceso:\n")
+console.log("Cantidad de productos:", productos.length);
+console.log("Nombre del segundo elemento del array:", productos[1].nombre);
+console.log("Nombre del cuarto elemento del array:", productos[3].nombre);
