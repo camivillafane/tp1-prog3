9eeb7ac205e4bcde7983c22d1caddbe125cd79f5
@ayelenrodriguez1610 <ayelenrodriguez1610@gmail.com>
2025-08-14 22:20:13 -0300

    punto3.2y4.1

--- gestionDatos.js
+++ gestionDatos.js
@@ -10,3 +10,16 @@ console.log("\nOperaciones básicas y Acceso:\n")
 console.log("Cantidad de productos:", productos.length);
 console.log("Nombre del segundo elemento del array:", productos[1].nombre);
 console.log("Nombre del cuarto elemento del array:", productos[3].nombre);
+
+//3-2. Recorre el array productos utilizando el método forEach() e imprimir la misma información que en el
+//punto anterior, pero agregando una frase descriptiva (ej. "Producto: [nombre], Precio: [precio]").
+productos.forEach(function(producto){
+    console.log(`El producto ${producto.nombre} tiene un costo de: ${producto.precio} `);
+});
+
+//4-1. Agregar dos elementos al final del array productos utilizando push().
+productos.push(
+    {id: 6, nombre: "Tacos", precio: 8500, stock: 20},
+    {id: 7, nombre: "Empanadas", precio: 11500, stock: 30}
+);
+console.log(productos);
